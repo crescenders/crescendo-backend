1d91d4547084f1c3eb494365f628825c57452e70
tgoddessana <twicegoddessana1229@gmail.com>
2023-03-02 16:19:38 +0900

    refactor : 사용되지 않는 import 제거

--- core/extensions.py
+++ core/extensions.py
@@ -1,7 +1,6 @@
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
-from flask_smorest import Api
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
