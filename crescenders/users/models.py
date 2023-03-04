97c43a4aa0d0cb0be01025ec2f85360f91f6a0ea
tgoddessana <twicegoddessana1229@gmail.com>
2023-03-04 18:48:41 +0900

    feat : users 앱 생성 && 모델 작성

---
+++ crescenders/users/models.py
@@ -0,0 +1,27 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from core.extensions import db
+from core.utils.basemodel import BaseModel, TimeStampedMixin, UUIDMixin
+
+
+class User(BaseModel, TimeStampedMixin, UUIDMixin):
+    email = db.Column(db.String(80), unique=True)
+    username = db.Column(db.String(10), unique=True)
+    first_name = db.Column(db.String(4))
+    last_name = db.Column(db.String(4))
+    gender = db.Column(db.Enum("남자", "여자"))
+    phone_number = db.Column(db.String(20))
+    password = db.Column(db.String(120))
+
+    def get_full_name(self):
+        """성 + 이름"""
+        return self.first_name + self.last_name
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return f"<id{self.id}, full_name:{self.get_full_name()}>"
