7ff779523c9fd705fa9dcc1f99058f87dfbf22cb
tgoddessana <twicegoddessana1229@gmail.com>
2023-03-04 20:58:31 +0900

    refactor : db 확장자명 변경

--- core/config/dev.py
+++ core/config/dev.py
@@ -2,4 +2,4 @@ from os.path import join
 
 from core.config.default import *
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(join(BASE_DIR, "db.sqlite"))
+SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(join(BASE_DIR, "db.sqlite3"))
