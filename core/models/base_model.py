042db2667442d3e9ad180c4ff053472f4f87ab1c
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-03 21:43:31 +0900

    refactor : mypy 에러 제거를 위한 주석 추가

--- core/models/base_model.py
+++ core/models/base_model.py
@@ -1,17 +1,11 @@
-from uuid import uuid4
+from flask_sqlalchemy import Model
 
 from core.extensions import db
 
 
-class BaseModel(db.Model):
+class BaseModel(db.Model):  # type: ignore[name-defined]
     """모든 모델의 부모 모델"""
 
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
-
-    @classmethod
-    @property
-    def __tablename__(cls):
-        """기본 테이블 이름을 클래스명으로 지정"""
-        return cls.__name__
