6dd86032145680271b66102cd34c68b49adab3ee
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-03 21:44:23 +0900

    refactor : UserService 인터페이스 / 구현체 추가

--- crescendo/users/services.py
+++ crescendo/users/services.py
@@ -1,25 +1,44 @@
+from abc import ABC, abstractmethod
+
 from crescendo.users.models import User
-from sqlalchemy.exc import IntegrityError
-from werkzeug.security import check_password_hash, generate_password_hash
 
 
-class UserService:
+class UserServiceABC(ABC):
+    @abstractmethod
+    def register(self):
+        pass
+
+    @abstractmethod
+    def get_lists(self):
+        pass
+
+    @abstractmethod
+    def get_one(self):
+        pass
+
+    @abstractmethod
+    def edit_info(self):
+        pass
+
+    @abstractmethod
+    def withdraw(self):
+        pass
+
+
+class UserService(UserServiceABC):
     """회원정보조회, 회원가입, 회원정보수정, 회원탈퇴, 검색"""
 
-    def create_user(self, **kwargs):
-        """사용자를 한 명 생성합니다.
-        비밀번호는 암호화되어 저장됩니다."""
-        kwargs.update({"password": generate_password_hash(kwargs.get("password"))})
+    def register(self):
+        pass
 
-    def get_all_users(self, **kwargs):
-        """모든 사용자 리스트를 반환합니다."""
-        return User.query.all()
+    def get_lists(self):
+        pass
 
-    def get_one_user(self, uuid):
-        """한 명의 사용자 인스턴스를 반환합니다."""
+    def get_one(self):
+        pass
 
-    def update_user(self):
-        """사용자를 특정하여 회원정보를 수정합니다."""
+    def edit_info(self):
+        pass
 
-    def withdraw(self, uuid):
-        """사용자를 특정하여 회원탈퇴를 진행합니다."""
+    def withdraw(self):
+        pass
