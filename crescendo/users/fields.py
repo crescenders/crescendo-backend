6a86b10684288165406064a05d32d7cb1040c400
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-03 19:47:50 +0900

    refactor : 데이터베이스 테이블명 변경

---
+++ crescendo/users/fields.py
@@ -0,0 +1,44 @@
+from flask_restx import fields
+
+from crescendo.users import users_api
+
+user = users_api.model(
+    "User",
+    {
+        "id": fields.Integer(
+            readonly=True,
+            example="1",
+            description="사용자의 데이터베이스 id",
+        ),
+        "uuid": fields.String(
+            required=True,
+            example="99cd6b7a-4b22-440a-8671-0c572d06926",
+            description="사용자의 데이터베이스 uuid",
+        ),
+        "email": fields.String(
+            required=True,
+            example="example@example.com",
+            description="사용자 계정 이메일",
+        ),
+        "username": fields.String(
+            required=True,
+            example="goddessana",
+            description="사용자 닉네임",
+        ),
+    },
+)
+
+
+user_list = users_api.model(
+    "UserList",
+    {
+        "total": fields.Integer(
+            readonly=True,
+        ),
+        "next": fields.String(),
+        "previous": fields.String(),
+        "users": fields.List(
+            fields.Nested(user),
+        ),
+    },
+)
