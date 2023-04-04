e776c37605f6cb6930e44458d5b04ecc93e3809f
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-04 19:27:54 +0900

    refactor : Swagger 문서 제목 변경

--- core/config/default.py
+++ core/config/default.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parents[2]
 PROPAGATE_EXCEPTIONS = True
 
-API_TITLE = "crescenders_backend API"
+API_TITLE = "crescendo_backend Server API"
 API_VERSION = "v1"
 OPENAPI_VERSION = "3.0.0"
 OPENAPI_URL_PREFIX = "/"
