99b78827a428c55cfa4382895b2defe3632bed73
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-05 23:48:46 +0900

    refactor : field 생성 시 dict() 사용하도록 변경

--- crescendo/users/fields.py
+++ crescendo/users/fields.py
@@ -4,44 +4,60 @@ from crescendo.users import users_api
 
 user = users_api.model(
     "User",
-    {
-        "id": fields.Integer(
+    dict(
+        id=fields.Integer(
             readonly=True,
             example="1",
             description="사용자의 데이터베이스 id",
         ),
-        "uuid": fields.String(
+        uuid=fields.String(
             required=True,
             example="99cd6b7a-4b22-440a-8671-0c572d06926",
             description="사용자의 데이터베이스 uuid",
         ),
-        "email": fields.String(
+        email=fields.String(
             required=True,
             example="example@example.com",
             description="사용자 계정 이메일",
         ),
-        "username": fields.String(
+        username=fields.String(
             required=True,
             example="goddessana",
             description="사용자 닉네임",
         ),
-    },
+    ),
 )
 
 user_list = users_api.model(
     "UserList",
-    {
-        "total": fields.Integer(
+    dict(
+        total_user_count=fields.Integer(
             readonly=True,
+            example="1",
+            description="전체 사용자 수",
         ),
-        "has_next": fields.String(
+        current_page=fields.Integer(
             readonly=True,
+            example="2",
+            description="현재 페이지",
         ),
-        "has_prev": fields.String(
+        total_page=fields.Integer(
             readonly=True,
+            example="15",
+            description="전체 페이지 수",
         ),
-        "users": fields.List(
+        has_next=fields.Boolean(
+            readonly=True,
+            example=True,
+            description="다음 페이지 존재 여부",
+        ),
+        has_prev=fields.Boolean(
+            readonly=True,
+            example=False,
+            description="이전 페이지 존재 여부",
+        ),
+        users=fields.List(
             fields.Nested(user),
         ),
-    },
+    ),
 )
