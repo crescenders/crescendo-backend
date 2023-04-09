192a2b6806397b3625b100e8d9481a2c09140f01
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-09 12:49:51 +0900

    refactor : mypy 검사 무시 라인 추가

--- core/extensions.py
+++ core/extensions.py
@@ -1,5 +1,5 @@
 from flask_jwt_extended import JWTManager
-from flask_marshmallow import Marshmallow
+from flask_marshmallow import Marshmallow  # type: ignore[import]
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
