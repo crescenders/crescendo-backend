c6e5aeb4ea861a84c7cebb4057d648cee06ff70f
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-17 16:26:08 +0900

    refactor : Entity, Repository 의존성 주입 처리

--- crescendo/auth/containers.py
+++ crescendo/auth/containers.py
@@ -1,5 +1,8 @@
 from dependency_injector import containers, providers
 
+from core.entities.pagination import PaginationEntity
+from crescendo.auth.entities import UserEntity
+from crescendo.auth.models import UserModel
 from crescendo.auth.repositories import UserRepositoryABC
 from crescendo.auth.services import UserServiceABC
 
@@ -7,14 +10,22 @@ from crescendo.auth.services import UserServiceABC
 class UserContainer(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(modules=["crescendo.auth.resources"])
 
+    # UserServiceABC 추상 클래스에 의존하도록 처리
     user_service_abc = providers.Dependency(
         instance_of=UserServiceABC  # type: ignore[type-abstract]
     )
+    # UserRepositoryABC 추상 클래스에 의존하도록 처리
     user_repository_abc = providers.Dependency(
         instance_of=UserRepositoryABC  # type: ignore[type-abstract]
     )
 
-    user_service = providers.Singleton(
+    user_service = providers.Singleton(  # UserService 는 Singleton 으로 주입
         user_service_abc,
-        user_repository=providers.Factory(user_repository_abc),
+        user_repository=providers.Factory(  # UserRepository 에 필요한 종속성 주입
+            user_repository_abc,
+            user_model_cls=providers.Object(UserModel),  # UserModel 클래스를 "있는 그대로" 반환
+            pagination_entity_factory=providers.Factory(PaginationEntity).provider,
+            user_entity_factory=providers.Factory(UserEntity).provider,
+        ),
+        user_entity_factory=providers.Factory(UserEntity).provider,
     )
