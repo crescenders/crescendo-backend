658719e41c5a7443aa3ccc3af5d0f4dad686542b
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-19 03:17:01 +0900

    refactor : 페이지네이션 스키마 응답 형식 이름 변경(next 가 python 내장 키워드와 이름이 겹침)

--- core/schemas/response.py
+++ core/schemas/response.py
@@ -3,5 +3,5 @@ from marshmallow import fields
 
 class PaginationResultSchemaMixin:
     count = fields.Integer(metadata={"description": "전체 아이템 개수"})
-    next = fields.URL(metadata={"description": "다음 페이지 URL"})
-    previous = fields.URL(metadata={"description": "이전 페이지 URL"})
+    next_num = fields.URL(metadata={"description": "다음 페이지 URL"})
+    previous_num = fields.URL(metadata={"description": "이전 페이지 URL"})
