a6fabbc3ed53ec0f03233335b39fb0de75b02ec5
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-19 03:15:06 +0900

    feat : JWT 발급 구현중

--- crescendo/auth/resources.py
+++ crescendo/auth/resources.py
@@ -1,16 +1,17 @@
-from copy import deepcopy
-from functools import wraps
-
 from dependency_injector.wiring import Provide, inject
 from flask import request
 from flask.views import MethodView
-from flask_jwt_extended import jwt_required
-from google.auth.transport import requests
-from google.oauth2 import id_token
+from google.auth.exceptions import GoogleAuthError  # type: ignore[import]
 
+from core.utils.jwt import jwt_required
 from crescendo.auth import auth_api
 from crescendo.auth.containers import UserContainer
-from crescendo.auth.schemas import UserListArgsSchema, UserListSchema, UserSchema
+from crescendo.auth.schemas import (
+    GoogleOauthArgsSchema,
+    UserListArgsSchema,
+    UserListSchema,
+    UserSchema,
+)
 
 
 @auth_api.route("/users/")
@@ -30,7 +31,12 @@ class UserListAPI(MethodView):
     @auth_api.arguments(UserListArgsSchema, location="query")
     @auth_api.response(200, UserListSchema)
     def get(self, kwargs):
-        """사용자 목록을 조회합니다."""
+        """
+        사용자 목록을 조회합니다.
+
+        STAFF, ADMIN 권한을 가진 사람만 회원정보를 조회할 수 있습니다.
+        Crescendo 서비스에서 발급된 JWT가 필요합니다
+        """
         return self.user_service.get_list(**kwargs)
 
 
@@ -50,44 +56,43 @@ class UserDetailAPI(MethodView):
 
     @auth_api.response(200, UserSchema)
     def get(self, user_uuid):
-        """UUID 로 특정되는 사용자 한 명의 정보를 조회합니다."""
+        """UUID 로 특정되는 사용자 한 명의 정보를 조회합니다.
+
+        본인, 혹은 STAFF, ADMIN 권한을 가진 사람만 회원정보를 조회할 수 있습니다.
+        Crescendo 서비스에서 발급된 JWT가 필요합니다."""
         return self.user_service.get_one_user(user_uuid)
 
     @auth_api.response(200, UserSchema)
     def put(self, user_uuid):
-        """UUID로 특정되는 사용자 한 명의 정보를 수정합니다."""
+        """UUID로 특정되는 사용자 한 명의 정보를 수정합니다.
+
+        본인, 혹은 STAFF, ADMIN 권한을 가진 사람만 회원정보를 수정할 수 있습니다.
+        Crescendo 서비스에서 발급된 JWT가 필요합니다."""
         return self.user_service.update_user(user_uuid, **request.json)
 
     @auth_api.response(204)
+    @jwt_required()
     def delete(self, user_uuid):
-        """UUID로 특정되는 사용자 한 명을 삭제합니다."""
+        """UUID로 특정되는 사용자 한 명을 삭제합니다.
+
+        본인, 혹은 STAFF, ADMIN 권한을 가진 사람만 회원탈퇴를 진행할 수 있습니다.
+        Crescendo 서비스에서 발급된 JWT가 필요합니다."""
         return self.user_service.withdraw(user_uuid)
 
 
-def jwt_required_with_doc(*args, **kwargs):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*f_args, **f_kwargs):
-            return jwt_required(*args, **kwargs)(func)(*f_args, **f_kwargs)
-
-        wrapper._apidoc = deepcopy(getattr(func, "_apidoc", {}))
-        wrapper._apidoc.setdefault("manual_doc", {})
-        wrapper._apidoc["manual_doc"]["security"] = [{"Bearer Auth": []}]
-        return wrapper
-
-    return decorator
-
-
 @auth_api.route("/login/google/", methods=["POST"])
-@jwt_required_with_doc()
-def google_login_api():
-    """Google 소셜 로그인을 진행합니다."""
-    # client_id = (
-    #     "560831292125-jnlpjp0chs024i3p8hn7ifr7uidigqok.apps.googleusercontent.com"
-    # )
-    # token = token.get("jwt")
-    # id_information = id_token.verify_oauth2_token(token, requests.Request(), client_id)
-    # return {
-    #     "Crescendo 서버에서 주는 ACCESS TOKEN": "이것은 Crescendo 서버에서 주는 Access Token!",
-    #     "Crescendo 서버에서 주는 REFRESH TOKEN": "이것은 Crescendo 서버에서 주는 Refresh Token!",
-    # }
+@auth_api.arguments(GoogleOauthArgsSchema)
+@inject
+def google_login_api(
+    google_oauth_token_data, user_service=Provide[UserContainer.user_service]
+):
+    """Google 소셜 로그인을 진행합니다.
+
+    Google 에서 발급된 JWT 가 필요합니다.
+    해당 JWT가 검증이 완료되면, 서버는 서비스 전용 JWT를 발급합니다.
+    만약 새로운 사용자라면, 회원가입 또한 진행합니다."""
+
+    google_oauth2_token = google_oauth_token_data.get("google_jwt")
+
+    res = user_service.oauth2_login(oauth2_provider="google", data=google_oauth2_token)
+    return res
