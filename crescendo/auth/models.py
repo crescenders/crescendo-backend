6e4ad65d233f08ff3090865e62b2f4f2a480b989
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-18 18:54:43 +0900

    feat : 권한 컬럼에 대한 유효성 검사기 추가

--- crescendo/auth/models.py
+++ crescendo/auth/models.py
@@ -1,18 +1,21 @@
 from enum import Enum
 
+from sqlalchemy.orm import validates
+
 from core.extensions import db
 from core.models.base_model import BaseModel
 from core.models.mixins import TimeStampedMixin, UUIDMixin
 
 
-class Roles(Enum):
-    ADMIN = 0
-    STAFF = 1
-    USER = 2
-
-
 class UserModel(BaseModel, TimeStampedMixin, UUIDMixin):
     __tablename__ = "user"
+
+    class Roles(Enum):
+        """사용자가 속한 권한을 정의합니다."""
+
+        ADMIN = 0
+        STAFF = 1
+        USER = 2
 
     email = db.Column(db.String(80), unique=True, nullable=False)
     username = db.Column(db.String(10), unique=True, nullable=False)
@@ -20,3 +23,13 @@ class UserModel(BaseModel, TimeStampedMixin, UUIDMixin):
 
     def __repr__(self) -> str:
         return f"<id:{self.id}, username:{self.username}>"
+
+    @validates("role")
+    def validate_role(self, key, value):
+        """role 컬럼에 대한 유효성 검사를 수행합니다."""
+        valid_roles = [role.name for role in UserModel.Roles]
+        if value not in valid_roles:
+            raise ValueError(
+                f"{value} is not a valid role, valid roles are {valid_roles}"
+            )
+        return value
