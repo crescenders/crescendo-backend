f3b2d29a6ca70864733650ea0dfbe4f7ca7fe943
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-19 00:31:03 +0900

    refactor : auth api blueprint 이름 수정

--- crescendo/auth/__init__.py
+++ crescendo/auth/__init__.py
@@ -6,8 +6,8 @@ from crescendo.auth.repositories import SQLAlchemyUserRepository
 from crescendo.auth.services import UserService
 
 auth_api = Blueprint(
-    name="Auth API",
-    import_name="auth_bp",
+    name="AuthAPI",
+    import_name=__name__,
     description="로그인, 회원가입, 사용자 정보 조회를 위한 API 입니다.",
 )
 
