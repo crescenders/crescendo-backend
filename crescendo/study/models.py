78ab1944a9fd8db92b88c2ceb5f3f0e2e8e2a578
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-23 00:14:00 +0900

    refactor : circle -> study

---
+++ crescendo/study/models.py
@@ -0,0 +1,10 @@
+from core.extensions import db
+from core.models.base_model import BaseModel
+from core.models.mixins import TimeStampedMixin, UUIDMixin
+
+
+class StudyModel(BaseModel, TimeStampedMixin, UUIDMixin):
+    __tablename__ = "study"
+
+    name = db.Column(db.String(80), nullable=False)
+    member_set = db.relationship("UserModel", backref="study")
