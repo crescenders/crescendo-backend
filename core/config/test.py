89eb4537bd6f8dda1e4828958fffd7bd43e1162a
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-25 22:35:08 +0900

    feat : 테스트 코드 수행 시 in-memory 에서 동작하도록 config 파일 작성

--- core/config/test.py
+++ core/config/test.py
@@ -1,8 +1,4 @@
-from os.path import join
-
 from core.config.default import *  # noqa: F403
 
 TESTING = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
-    join(BASE_DIR, "test_db.sqlite3")  # noqa: F405
-)
+SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
