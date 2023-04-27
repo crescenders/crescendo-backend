d899238accbdfd9bdf8922f867a7174cb53a9d5e
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-28 01:09:18 +0900

    feat : 타입힌트 수정 / 타입 이름 DBEntity 로  리팩토링

--- core/repositories/base.py
+++ core/repositories/base.py
@@ -1,14 +1,14 @@
 from typing import Generic, Type
 
-from core.repositories.type import T
+from core.repositories.type import DBEntity
 
 
-class BaseRepository(Generic[T]):
+class BaseRepository(Generic[DBEntity]):
     """
     The Base Repository class of all Repositories.
     """
 
-    def __init__(self, entity: Type[T]):
+    def __init__(self, entity: Type[DBEntity]):
         """
         :param entity: The types of entities you want to cover in this repository.
         """
