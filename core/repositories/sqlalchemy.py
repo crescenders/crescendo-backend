57862a1c853d788180d300337e6852288affb8a5
tgoddessana <twicegoddessana1229@gmail.com>
2023-04-28 01:08:13 +0900

    feat : SQLALchemyRepository 상세구현 추가

--- core/repositories/sqlalchemy.py
+++ core/repositories/sqlalchemy.py
@@ -1,62 +1,141 @@
-from typing import List, Optional, Type
+from typing import List, Optional
 
-from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow.sqla import SQLAlchemyAutoSchema  # type: ignore[import]
+from sqlalchemy import inspect, select
+from sqlalchemy.orm import Query
 
-from core.repositories.crud import CRUDRepositoryABC
-from core.repositories.type import T
+from core.repositories.condition import FilteringSortingPaginationRepository
+from core.repositories.type import DBEntity
 
 
-class SQLAlchemyRepositoryABC(CRUDRepositoryABC):
+class SQLAlchemyFullRepository(FilteringSortingPaginationRepository):
     """
     The implementation of CRUDRepositoryABC, with SQLAlchemy.
 
     this implementation has dependency with flask-sqlalchemy's SQLAlchemy object.
     """
 
-    def __init__(
-        self, entity: Type[T], eid_name: str, db: SQLAlchemy, sqlalchemy_model
-    ):
+    def __init__(self, entity: type[DBEntity], db, sqlalchemy_model):
         """
-        :type db: flask-sqlalchemy.SQLAlchemy
+        :param db: flask-sqlalchemy.SQLAlchemy
         :param sqlalchemy_model: the SQLAlchemy model
         """
         self.db = db
         self.sqlalchemy_model = sqlalchemy_model
-        super().__init__(entity, eid_name)
+        super().__init__(entity=entity)
+        assert set(self.entity.__annotations__.keys()) == set(
+            [column.name for column in self.sqlalchemy_model.__table__.columns]
+        ), (
+            "sqlalchemy model fields not match entity fields."
+            f"{self.entity.__annotations__.keys()} "
+            f"!= "
+            f"{[column.name for column in self.sqlalchemy_model.__table__.columns]}"
+        )
 
-    def save(self, entity: T) -> T:
+    def save(self, entity: DBEntity) -> DBEntity:
         with self.db.session.begin():
-            self.db.session.add(self._entity_to_model(self.entity))
+            model_instance = self._entity_to_sqlalchemy_model(entity)
+            self.db.session.add(model_instance)
+            self.db.session.commit()
+        return self._sqlalchemy_model_to_entity(model_instance)
 
-    def save_all(self, entities: List[T]) -> List[T]:
-        pass
+    def save_all(self, entities: List[DBEntity]) -> List[DBEntity]:
+        # TODO : 속도 개선
+        model_instances = [
+            self._entity_to_sqlalchemy_model(entity) for entity in entities
+        ]
+        with self.db.session.begin():
+            for model_instance in model_instances:
+                self.db.session.add(model_instance)
+            self.db.session.commit()
+        return [
+            self._sqlalchemy_model_to_entity(model_instance)
+            for model_instance in model_instances
+        ]
 
-    def read_by_eid(self) -> Optional[T]:
-        pass
+    def read_by_id(self, id) -> Optional[DBEntity]:
+        if id is None:
+            raise ValueError("id cannot be None.")
+        query_result = self.db.session.get(self.sqlalchemy_model, id)
+        if query_result:
+            return self._sqlalchemy_model_to_entity(query_result)
+        return None
 
-    def is_exists_by_eid(self) -> bool:
-        pass
+    def is_exists_by_id(self, id) -> bool:
+        if id is None:
+            raise ValueError("id cannot be None.")
+        return bool(self.db.session.get(self.sqlalchemy_model, id))
 
-    def read_all(self) -> List[T]:
-        pass
+    def read_all(
+        self, pagination_entity, filtering_entity, sorting_entity
+    ) -> List[Optional[DBEntity]]:
+        return [
+            self._sqlalchemy_model_to_entity(query_result)
+            for query_result in self.db.session.execute(select(self.sqlalchemy_model))
+            .scalars()
+            .all()
+        ]
 
-    def read_all_by_eids(self, ids: List[int]) -> List[T]:
-        pass
+    def read_all_by_ids(self, ids: List[int]) -> List[Optional[DBEntity]]:
+        return [self.read_by_id(_id) for _id in ids]
 
     def count(self) -> int:
-        pass
+        return self.db.session.query(self.sqlalchemy_model).count()
 
-    def delete_by_eid(self, eid: int) -> None:
-        pass
+    def delete_by_id(self, id: int) -> None:
+        model_instance = self.db.session.get(self.sqlalchemy_model, id)
+        if model_instance:
+            self.db.session.delete(self.db.session.get(self.sqlalchemy_model, id))
+            self.db.session.commit()
+        else:
+            raise ValueError(f"{self.sqlalchemy_model} with id {id} not found.")
 
-    def delete(self, entity: T) -> None:
-        pass
+    def delete(self, entity: DBEntity) -> None:
+        model_instance = self.db.session.get(self.sqlalchemy_model, entity.id)
+        if not model_instance:
+            raise ValueError(
+                f"{self.sqlalchemy_model} with entity {entity} not found.\n"
+                f"make sure the entity instance is stored in database."
+            )
+        self.db.session.delete(model_instance)
+        self.db.session.commit()
 
-    def delete_all_by_eids(self, ids: List[int]) -> None:
-        pass
+    def delete_all_by_ids(self, ids: List[int]) -> None:
+        self.db.session.query(self.sqlalchemy_model).filter(
+            self.sqlalchemy_model.id.in_(ids)
+        ).delete()
 
     def delete_all(self) -> None:
-        pass
+        self.sqlalchemy_model.query.delete()
 
-    def _sqlalchemy_model_to_entity(self, sqlalchemy_model) -> T:
+    def _get_base_query(self) -> Query:
+        return self.db.session.query(self.sqlalchemy_model)
+
+    def _sqlalchemy_model_to_entity(self, sqlalchemy_instance) -> DBEntity:
         """convert sqlalchemy model to entity."""
+        assert isinstance(
+            sqlalchemy_instance, self.sqlalchemy_model
+        ), f"{sqlalchemy_instance} is not {self.sqlalchemy_model}"
+        sqlalchemy_model_pk_names = [
+            pk.name for pk in inspect(self.sqlalchemy_model).primary_key
+        ]
+        if len(sqlalchemy_model_pk_names) == 1:
+            dump_data = self._get_schema().dump(sqlalchemy_instance)
+            return self.entity(**dump_data)
+        else:
+            raise ValueError("multi-pk case is not supported in current version.")
+            # TODO : handle multi-pk case
+
+    def _entity_to_sqlalchemy_model(self, entity):
+        """convert entity to sqlalchemy model."""
+        assert isinstance(
+            entity, self.entity
+        ), f"{entity} is not instance of {self.entity}"
+        return self.sqlalchemy_model(**self._get_schema().dump(entity))
+
+    def _get_schema(self):
+        class SQLAlchemyModelSchema(SQLAlchemyAutoSchema):
+            class Meta:
+                model = self.sqlalchemy_model
+
+        return SQLAlchemyModelSchema()
