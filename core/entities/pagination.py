1735966bf5dc2b9fefc210f8028daeaaf794467e
tgoddessana <twicegoddessana1229@gmail.com>
2023-05-07 14:27:23 +0900

    refactor : PaginationResponseEntity Dataclass로 변경

--- core/entities/pagination.py
+++ core/entities/pagination.py
@@ -1,17 +1,12 @@
+from dataclasses import dataclass
 from typing import Generic, List, Optional, TypeVar
 
 T = TypeVar("T")
 
 
+@dataclass
 class PaginationResponseEntity(Generic[T]):
-    def __init__(
-        self,
-        count: int,
-        next_num: Optional[int],
-        previous_num: Optional[int],
-        results: List[T],
-    ):
-        self.count = count
-        self.next_num = next_num
-        self.previous_num = previous_num
-        self.results = results
+    count: int
+    next_page: Optional[str]
+    previous_page: Optional[str]
+    results: List[T]
