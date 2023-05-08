991954b12f75baddf5de075a93691e4133f195ab
tgoddessana <twicegoddessana1229@gmail.com>
2023-05-08 22:51:08 +0900

    refactor: 기본 sorting 스키마 정의

--- core/schemas/sorting.py
+++ core/schemas/sorting.py
@@ -1,5 +1,7 @@
 from flask_marshmallow import Schema  # type: ignore[import]
+from marshmallow import post_load
 
+from core.entities.sorting import SortingRequest
 from core.schemas import fields
 
 
@@ -7,3 +9,7 @@ class SortingRequestSchema(Schema):
     sort_by = fields.Sorting(
         metadata={"description": "정렬 조건입니다. `필드명:정렬조건(asc or desc)` 형식을 준수해야 합니다."}
     )
+
+    @post_load
+    def to_entity(self, data, **kwargs) -> SortingRequest:
+        return SortingRequest(**data)
