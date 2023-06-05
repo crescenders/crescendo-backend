3720dcccf9edca090b0562de49ca072cf67142fb
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-05 21:31:48 +0900

    refactor : @staticmethod 코드 추가

--- core/repositories/sqlalchemy.py
+++ core/repositories/sqlalchemy.py
@@ -189,6 +189,7 @@ class SQLAlchemyFullRepository(CRUDRepositoryABC, Generic[T]):
 
         return SQLAlchemyModelSchema()
 
+    @staticmethod
     def _update_model_with_entity(self, model_instance, entity):
         # Iterate over the fields of the entity object
         for field_name, field_value in entity.__dict__.items():
