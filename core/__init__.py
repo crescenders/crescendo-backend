e8bc9f2ec34c1d2b08608d4b59f10842f4fb2797
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-06 02:10:23 +0900

    refactor : frf 수정된 버전으로 업데이트, 기본 extensions 파일 추가

--- core/__init__.py
+++ core/__init__.py
@@ -5,7 +5,7 @@ class CrescendoApplicationFactory(BaseApplicationFactory):
     FLASK_APP_NAME = "crescendo-backend"
     APP_BASE_DIR = "crescendo"
     CONFIG_FILE = "core.config.dev"
-    EXTENSION_FILE = "core.factory.extensions"
+    EXTENSION_FILE = "fullask_rest_framework.factory.extensions"
     MICRO_APP_CONFIG = [
         {"crescendo.auth": "/api/v1/auth/"},
     ]
