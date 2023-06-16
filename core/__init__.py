b94ade3f9b731848551cc5e3d2a9f6c10ac98c69
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-16 19:44:31 +0900

    feat : frf update로 테스트 및 개발환경 분리

--- core/__init__.py
+++ core/__init__.py
@@ -4,7 +4,15 @@ from fullask_rest_framework.factory.app_factory import BaseApplicationFactory
 class CrescendoApplicationFactory(BaseApplicationFactory):
     FLASK_APP_NAME = "crescendo-backend"
     APP_BASE_DIR = "crescendo"
-    CONFIG_MODULE = "core.config.dev"
+    DOTENV_SETTINGS = {"dotenv_path": ".env"}
+    CONFIG_MAPPING = {
+        "dev": {
+            "from_object": {"obj": "core.config.dev"},
+        },
+        "test": {
+            "from_object": {"obj": "core.config.test"},
+        },
+    }
     EXTENSION_MODULE = "fullask_rest_framework.factory.extensions"
     MICRO_APP_CONFIG = [
         {"crescendo.auth": "/api/v1/auth/"},
