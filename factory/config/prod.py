c8a079a132c30a249112016e128f66073ba8c470
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-20 02:59:50 +0900

    feat : 로깅 설정

--- factory/config/prod.py
+++ factory/config/prod.py
@@ -1,7 +1,40 @@
+from logging.config import dictConfig
 from os.path import join
 
 from factory.config.default import *  # noqa: F403
 
+DEBUG = False
+
 SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
     join(BASE_DIR, "db.sqlite3")  # noqa: F405
+)
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s %(levelname)s - %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            },
+            "file": {
+                "level": "INFO",
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": os.path.join(BASE_DIR, "logs/error.log"),
+                "maxBytes": 1024 * 1024 * 5,  # 5 MB
+                "backupCount": 5,
+                "formatter": "default",
+            },
+        },
+        "root": {
+            "level": "INFO",
+            "handlers": ["wsgi", "file"],
+        },
+    }
 )
