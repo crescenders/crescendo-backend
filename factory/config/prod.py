e44179ea24d4d0a1af709c43c96855f7e6438ddc
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-21 20:53:40 +0900

    fix : dictconfig 설정 삭제

--- factory/config/prod.py
+++ factory/config/prod.py
@@ -1,4 +1,3 @@
-from logging.config import dictConfig
 from os.path import join
 
 from factory.config.default import *  # noqa: F403
@@ -7,29 +6,4 @@ DEBUG = False
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
     join(BASE_DIR, "db.sqlite3")  # noqa: F405
-)
-
-dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": "%(asctime)s %(levelname)s - %(module)s: %(message)s",
-            }
-        },
-        "handlers": {
-            "file": {
-                "level": "INFO",
-                "class": "logging.handlers.RotatingFileHandler",
-                "filename": os.path.join(BASE_DIR, "logs/error.log"),
-                "maxBytes": 1024 * 1024 * 5,  # 5 MB
-                "backupCount": 5,
-                "formatter": "default",
-            },
-        },
-        "root": {
-            "level": "INFO",
-            "handlers": ["file"],
-        },
-    }
 )
