66c75a548030b19db79e2bbee3ea30e2a6e1e2ed
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-21 20:51:52 +0900

    refactor : 개발용 설정에 DEBUG 추가

--- factory/config/dev.py
+++ factory/config/dev.py
@@ -2,6 +2,8 @@ from os.path import join
 
 from factory.config.default import *  # noqa: F403
 
+DEBUG = True
+
 SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
     join(BASE_DIR, "db.sqlite3")  # noqa: F405
 )
