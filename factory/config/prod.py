01c28b9cc755dba38d1dfd3592c4b799a2f1606d
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-22 23:54:46 +0900

    feat : oracle 자율운영 데이터베이스 접속 설정

--- factory/config/prod.py
+++ factory/config/prod.py
@@ -1,9 +1,13 @@
-from os.path import join
+import os
+from urllib.parse import quote_plus
 
 from factory.config.default import *  # noqa: F403
 
 DEBUG = False
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
-    join(BASE_DIR, "db.sqlite3")  # noqa: F405
+SQLALCHEMY_DATABASE_URI = (
+    f"oracle+oracledb://{quote_plus(os.getenv('DB_USERNAME'))}:{quote_plus(os.getenv('DB_PASSWORD'))}@{os.getenv('DB_DSN')}"
+    f"?config_dir={quote_plus(os.getenv('DB_CONFIG_DIR'))}"
+    f"&wallet_location={quote_plus(os.getenv('DB_WALLET_LOCATION'))}"
+    f"&wallet_password={quote_plus(os.getenv('DB_WALLET_PASSWORD'))}"
 )
