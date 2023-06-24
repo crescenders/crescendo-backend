3ec0fc30361273155efc52cdee7fa8e6395ad188
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-24 18:21:04 +0900

    feat : 배포용 앱 파일 분리

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from factory import CrescendoApplicationFactory
 
-dev_app = CrescendoApplicationFactory.create_app("dev")
-prod_app = CrescendoApplicationFactory.create_app("prod")
+app = CrescendoApplicationFactory.create_app("prod")
 
 if __name__ == "__main__":
-    dev_app.run(
+    app.run(
         debug=True,
         host="0.0.0.0",
     )
