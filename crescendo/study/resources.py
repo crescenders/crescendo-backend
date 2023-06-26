31c20c891b3fdf8d681e426d8c671aa91606f7a6
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-26 19:17:09 +0900

    feat : 카테고리 api 개발중

--- crescendo/study/resources.py
+++ crescendo/study/resources.py
@@ -1,10 +1,12 @@
 #########################
 # Define your Blueprint.#
 #########################
+from dependency_injector.wiring import Provide, inject
 from flask.views import MethodView
 from flask_smorest import Blueprint
 
 from crescendo.study.schemas import CategoryListSchema
+from crescendo.study.services import CategoryServiceABC
 
 study_bp = Blueprint(
     name="StudyAPI",
@@ -16,26 +18,38 @@ study_bp = Blueprint(
 
 @study_bp.route("/categories/", tags=["CategoryAPI"])
 class CategoryListAPI(MethodView):
+    @inject
+    def __init__(
+        self, category_service: CategoryServiceABC = Provide["category_service"]
+    ):
+        self.category_service = category_service
+
     @study_bp.response(200, CategoryListSchema(many=True))
     def get(self):
         """카테고리 목록을 조회합니다."""
-        pass
+        return self.category_service.get_all()
 
     @study_bp.response(201, CategoryListSchema())
     def post(self):
         """새로운 카테고리를 생성합니다."""
-        pass
+        return self.category_service.create()
 
 
 @study_bp.route("/categories/<int:category_id>", tags=["CategoryAPI"])
 class CategoryDetailAPI(MethodView):
+    @inject
+    def __init__(
+        self, category_service: CategoryServiceABC = Provide["category_service"]
+    ):
+        self.category_service = category_service
+
     def put(self):
         """ID 로 특정되는 카테고리 정보를 수정합니다."""
-        pass
+        return self.category_service.edit()
 
     def delete(self):
         """ID 로 특정되는 카테고리를 하나 삭제합니다."""
-        pass
+        return self.category_service.delete()
 
 
 @study_bp.route("/studies/posts/", tags=["StudyAPI"])
