b260f1926c02be481e266227a0978ebc179f9f7e
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-26 23:00:31 +0900

    feat : FRF 업데이트로 class 기반으로 앱 등록

--- crescendo/auth/__init__.py
+++ crescendo/auth/__init__.py
@@ -1,12 +1,15 @@
 from dependency_injector import providers
+from fullask_rest_framework.factory.microapp import MicroApp
 
 from crescendo.auth.containers import UserContainer
 from crescendo.auth.repositories import FullUserRepository
 from crescendo.auth.resources import auth_bp
 from crescendo.auth.services import UserService
 
-BLUEPRINT = auth_bp
-MICROAPP_CONTAINER = UserContainer(
-    user_service_abc=providers.Factory(UserService),
-    user_repository_abc=providers.Factory(FullUserRepository),
-)
+
+class AuthMicroApp(MicroApp):
+    blueprint = auth_bp
+    microapp_container = UserContainer(
+        user_service_abc=providers.Factory(UserService),
+        user_repository_abc=providers.Factory(FullUserRepository),
+    )
--- crescendo/study/__init__.py
+++ crescendo/study/__init__.py
@@ -1,12 +1,15 @@
 from dependency_injector import providers
+from fullask_rest_framework.factory.microapp import MicroApp
 
 from crescendo.study.containers import StudyContainer
 from crescendo.study.repositories import SQLAlchemyFullCategoryRepository
 from crescendo.study.resources import study_bp
 from crescendo.study.services import CategoryService
 
-BLUEPRINT = study_bp
-MICROAPP_CONTAINER = StudyContainer(
-    category_service_abc=providers.Factory(CategoryService),
-    category_repository_abc=providers.Factory(SQLAlchemyFullCategoryRepository),
-)
+
+class StudyMicroApp(MicroApp):
+    blueprint = study_bp
+    microapp_container = StudyContainer(
+        category_service_abc=providers.Factory(CategoryService),
+        category_repository_abc=providers.Factory(SQLAlchemyFullCategoryRepository),
+    )
