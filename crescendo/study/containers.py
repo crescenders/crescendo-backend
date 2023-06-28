e5ad8407ae4f4ec6e08272e7f980f28a970d84bf
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-29 00:40:55 +0900

    fix : nonetype error 해결

--- crescendo/study/containers.py
+++ crescendo/study/containers.py
@@ -1,8 +1,6 @@
 from dependency_injector import containers, providers
 from fullask_rest_framework.factory.extensions import db
 
-from crescendo.study.entities import CategoryEntity
-from crescendo.study.models import CategoryModel
 from crescendo.study.repositories import SQLAlchemyFullCategoryRepositoryABC
 from crescendo.study.services import CategoryServiceABC
 
@@ -11,11 +9,9 @@ class StudyContainer(containers.DeclarativeContainer):
     category_service_abc = providers.Dependency(
         instance_of=CategoryServiceABC
     )  # type: ignore[type-abstract]
-
     category_repository_abc = providers.Dependency(
         instance_of=SQLAlchemyFullCategoryRepositoryABC
     )  # type: ignore[type-abstract]
-
     category_repository = providers.Singleton(
         category_repository_abc,
         db=providers.Object(db),
