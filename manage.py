8351879c6fd33bd89a1dc678d5604d2bd8dc312e
tgoddessana <twicegoddessana1229@gmail.com>
2023-08-04 01:32:20 +0900

    refactor: manage.py 원래 설정대로 dev 환경이 우선하도록 설정

--- manage.py
+++ manage.py
@@ -14,11 +14,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    try:
-        execute_from_command_line(sys.argv)
-    except ModuleNotFoundError:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
-        execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
