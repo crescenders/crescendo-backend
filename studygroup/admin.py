2291a8bfc79b5920117daac93db2a83f5c41c897
tgoddessana <twicegoddessana1229@gmail.com>
2023-08-09 23:13:21 +0900

    feat: 카테고리, 태그, 스터디 모델 관리자 페이지 등록

--- studygroup/admin.py
+++ studygroup/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from studygroup import models
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.StudyGroup)
+class StudyGroupAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "user_limit",
+        "start_date",
+        "end_date",
+        "deadline",
+        "title",
+        "content",
+        "leader",
+    )
