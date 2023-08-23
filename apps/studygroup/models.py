11e4b8a6e67e63c76d42a48f4fa332a621a43362
tgoddessana <twicegoddessana1229@gmail.com>
2023-08-24 00:41:11 +0900

    refactor: 스터디그룹 모델 파일 재작성 및 마이그레이션

--- apps/studygroup/models.py
+++ apps/studygroup/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from apps.accounts.models import User
@@ -38,42 +39,11 @@ class StudyGroup(TimestampedModel):
 
     # StudyGroup Fields
     name = models.CharField(max_length=80)
-    user_limit = models.PositiveSmallIntegerField()
+    member_limit = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(2), MaxValueValidator(10)]
+    )
     start_date = models.DateField()
     end_date = models.DateField()
-
-    # Foreign Keys
-    categories = models.ManyToManyField(Category, related_name="study_groups")
-    tags = models.ManyToManyField(Tag, related_name="study_groups", blank=True)
-
-    def __str__(self):
-        return f"<StudyGroup {self.name}>"
-
-
-class StudyGroupMember(models.Model):
-    class Meta:
-        verbose_name = "StudyGroup Members"
-        verbose_name_plural = "StudyGroup Members"
-
-    is_leader = models.BooleanField(default=False)
-    studygroup = models.ForeignKey(
-        StudyGroup, on_delete=models.CASCADE, related_name="studygroup_members"
-    )
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="studygroup_members"
-    )
-
-    def __str__(self):
-        return f"""<StudyGroup "{self.studygroup}", {self.user.username}>"""
-
-
-class PromotionPost(TimestampedModel):
-    class Meta:
-        verbose_name = "Promotion Post"
-        verbose_name_plural = "Promotion Posts"
-
-    # UUID Key
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     # Post Fields
     head_image = models.ImageField(upload_to="studygroup/head_image", blank=True)
@@ -82,8 +52,14 @@ class PromotionPost(TimestampedModel):
     deadline = models.DateField()
 
     # Foreign Keys
-    study_group = models.OneToOneField("StudyGroup", on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    leader = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="managing_study_groups"
+    )
+    members = models.ManyToManyField(
+        User, related_name="joined_study_groups", blank=True
+    )
+    categories = models.ManyToManyField(Category, related_name="study_groups")
+    tags = models.ManyToManyField(Tag, related_name="study_groups", blank=True)
 
     def __str__(self):
-        return f"<Promotion Post {self.title}>"
+        return f"<StudyGroup {self.name}>"
