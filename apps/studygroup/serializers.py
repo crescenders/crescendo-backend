955b01a8bad848862f9aef0fa2aa2aafa7e0c89b
tgoddessana <twicegoddessana1229@gmail.com>
2023-08-24 00:41:36 +0900

    feat: 스터디그룹 개설 및 목록 조회 API 개발

--- apps/studygroup/serializers.py
+++ apps/studygroup/serializers.py
@@ -1,4 +1,6 @@
+from django.utils.datetime_safe import date
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from apps.studygroup import models
 
@@ -9,31 +11,66 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ["name"]
 
 
-class PromotionPostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.PromotionPost
-        fields = "__all__"
-
-
-class StudyGroupSerializer(serializers.ModelSerializer):
-    posts = PromotionPostSerializer(many=True)
+class StudyGroupListSerializer(serializers.ModelSerializer):
+    leader_username = serializers.CharField(source="leader.username", read_only=True)
+    post_title = serializers.CharField(source="title")
+    post_content = serializers.CharField(source="content", write_only=True)
+    study_name = serializers.CharField(source="name")
+    start_date = serializers.DateField(write_only=True)
+    end_date = serializers.DateField(write_only=True)
+    deadline = serializers.DateField(write_only=True)
+    until_deadline = serializers.SerializerMethodField(read_only=True)
+    current_member_count = serializers.IntegerField(
+        source="members.count", read_only=True
+    )  # 현재 인원수를 나타냅니다.
     categories = serializers.SlugRelatedField(
-        many=True, read_only=True, slug_field="name"
+        slug_field="name",
+        many=True,
+        queryset=models.Category.objects.all(),
     )
     tags = serializers.SlugRelatedField(
-        many=True, queryset=models.Tag.objects.all(), slug_field="name"
+        slug_field="name", many=True, queryset=models.Tag.objects.all()
     )
+    _links = serializers.SerializerMethodField()
 
     class Meta:
         model = models.StudyGroup
         fields = [
-            "uuid",
-            "name",
-            "user_limit",
+            "head_image",
+            "leader_username",
+            "post_title",
+            "post_content",
+            "study_name",
             "start_date",
             "end_date",
-            "posts",
-            "categories",
+            "deadline",
+            "until_deadline",
             "tags",
-            "studygroup_members",
+            "categories",
+            "current_member_count",
+            "member_limit",
+            "until_deadline",
+            "_links",
         ]
+
+    @staticmethod
+    def get_until_deadline(obj):
+        return (obj.deadline - date.today()).days
+
+    def get__links(self, obj):
+        request = self.context["request"]
+        links = {
+            "self": {
+                "href": reverse(
+                    "studygroup_detail", kwargs={"uuid": obj.uuid}, request=request
+                )
+            },
+            "leader": {
+                "href": reverse(
+                    "user_profile_uuid",
+                    kwargs={"uuid": obj.leader.uuid},
+                    request=request,
+                )
+            },
+        }
+        return links
--- apps/studygroup/views.py
+++ apps/studygroup/views.py
@@ -1,8 +1,10 @@
 from typing import Any
 
+from django.db.models import QuerySet
 from drf_spectacular.utils import extend_schema
 from rest_framework import generics, viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -12,15 +14,27 @@ from apps.studygroup import models, serializers
 @extend_schema(tags=["스터디그룹 API"])
 class StudyGroupAPISet(viewsets.ModelViewSet):
     lookup_field = "uuid"
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = models.StudyGroup.objects.all()
-    serializer_class = serializers.StudyGroupSerializer
+    parser_classes = (MultiPartParser, FormParser)
+    serializer_class = serializers.StudyGroupListSerializer
+
+    def get_serializer_class(self):
+        if self.action in ["list", "create"]:
+            return serializers.StudyGroupListSerializer
+        # elif self.action in ["retrieve", "update", "partial_update", "destroy"]:
+        #     return serializers.StudyGroupDetailSerializer
+        return super().get_serializer_class()
+
+    def perform_create(self, serializer):
+        serializer.validated_data["leader"] = self.request.user
+        return super().perform_create(serializer)
 
     @extend_schema(summary="스터디그룹 홍보글 목록을 조회합니다.")
     def list(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         return super().list(request, *args, **kwargs)
 
-    @extend_schema(summary="create")
+    @extend_schema(summary="새로운 스터디그룹을 개설합니다.")
     def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         return super().create(request, *args, **kwargs)
 
