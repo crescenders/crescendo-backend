d4db351b4efd26733074f3225771809b30223c7e
tgoddessana <twicegoddessana1229@gmail.com>
2023-08-25 03:37:47 +0900

    feat: 스터디그룹 멤버 모델 작성, 재사용 가능한 메서드 추가 `is_closed`, `leaders`, `current_member_count`

--- apps/studygroup/admin.py
+++ apps/studygroup/admin.py
@@ -14,17 +14,70 @@ class TagAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(models.StudyGroupMember)
+class StudyGroupMemberAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(models.StudyGroup)
 class StudyGroupAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid", "created_at", "updated_at")
+    fieldsets = (
+        (
+            "메타 정보",
+            {
+                "fields": (
+                    "uuid",
+                    "name",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+        (
+            "게시물 정보",
+            {
+                "fields": (
+                    "head_image",
+                    "title",
+                    "content",
+                    "categories",
+                    "tags",
+                )
+            },
+        ),
+        (
+            "모집 정보",
+            {
+                "fields": (
+                    "start_date",
+                    "end_date",
+                    "deadline",
+                    "member_limit",
+                )
+            },
+        ),
+    )
+
     list_display = (
         "name",
         "member_limit",
-        "leader",
         "is_closed",
+        "current_member_count",
         "deadline",
     )
 
     def is_closed(self, instance):
-        return timezone.now().date() > instance.deadline
+        return instance.is_closed
 
     is_closed.boolean = True
+
+    class StudyGroupMemberInline(admin.TabularInline):
+        verbose_name = "스터디그룹 멤버"
+        verbose_name_plural = "스터디그룹 멤버들"
+        model = models.StudyGroupMember
+        extra = 0
+
+    inlines = [
+        StudyGroupMemberInline,
+    ]
