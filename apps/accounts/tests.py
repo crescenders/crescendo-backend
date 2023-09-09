fe83944e92d8d3784eaedf7f3629d45a9e5224c2
goddessana <twicegoddessana1229@gmail.com>
2023-09-10 04:01:37 +0900

    test: refresh token 테스트 코드 작성 #35

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+from apps.accounts.models import User
+
+REFRESH_API_URL_NAME = "token_refresh"
+
+
+class TokenRefreshTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="test@test.com",
+            username="test",
+            password="test",
+        )
+
+    def test_token_refresh(self):
+        """
+        Token Refresh API 를 통해 access token 을 새로 발급받을 수 있어야 합니다.
+        """
+        refresh_token = RefreshToken.for_user(self.user)
+        response = self.client.post(
+            path=reverse(REFRESH_API_URL_NAME),
+            data={"refresh": str(refresh_token)},
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
+        self.assertIn("refresh", response.data)
