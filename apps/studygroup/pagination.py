339f62f7e46f82a949b95630b5cae94a8984fe44
goddessana <twicegoddessana1229@gmail.com>
2023-09-22 00:36:20 +0900

    refactor: PAGE_SIZE 를 클래스 안에서 지정

--- apps/studygroup/pagination.py
+++ apps/studygroup/pagination.py
@@ -2,6 +2,7 @@ from rest_framework.pagination import CursorPagination
 
 
 class StudyGroupPagination(CursorPagination):
+    page_size = 12
     cursor_query_param = "cursor"
     cursor_query_description = "커서 값입니다."
     invalid_cursor_message = "잘못된 커서 값입니다."
