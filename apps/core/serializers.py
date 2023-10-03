f94ff98c181f7806bf79c44860c5b2a0abdfe817
goddessana <twicegoddessana1229@gmail.com>
2023-10-03 22:17:16 +0900

    refactor: core/serializers 에 타입 힌트 추가

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -1,12 +1,15 @@
+from typing import Any
+
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Model
 from rest_framework import serializers
 
 
 class CreatableSlugRelatedField(serializers.SlugRelatedField):
-    def to_internal_value(self, data):
+    def to_internal_value(self, data: Any) -> Model:
         try:
-            return self.get_queryset().get(**{self.slug_field: data})
+            return self.get_queryset().get(**{str(self.slug_field): data})
         except ObjectDoesNotExist:
-            return self.get_queryset().create(**{self.slug_field: data})
+            return self.get_queryset().create(**{str(self.slug_field): data})
         except (TypeError, ValueError):
             self.fail("invalid")
