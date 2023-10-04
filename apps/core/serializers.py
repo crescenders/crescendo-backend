bd221cb8d68e7de66c1607903a618d8398beb117
goddessana <twicegoddessana1229@gmail.com>
2023-10-04 09:55:55 +0900

    refactor: type hint 검사 통과하도록 추가 #92

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -8,8 +8,12 @@ from rest_framework import serializers
 class CreatableSlugRelatedField(serializers.SlugRelatedField):
     def to_internal_value(self, data: Any) -> Model:
         try:
-            return self.get_queryset().get(**{str(self.slug_field): data})
+            result = self.get_queryset().get(**{str(self.slug_field): data})
+            assert isinstance(result, Model)
+            return result
         except ObjectDoesNotExist:
-            return self.get_queryset().create(**{str(self.slug_field): data})
+            result = self.get_queryset().create(**{str(self.slug_field): data})
+            assert isinstance(result, Model)
+            return result
         except (TypeError, ValueError):
             self.fail("invalid")
