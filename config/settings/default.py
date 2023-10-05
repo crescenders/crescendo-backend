8b7a3e14c8de6269a6a84439d69c26af7a94fb9d
goddessana <twicegoddessana1229@gmail.com>
2023-10-05 22:49:06 +0900

    fix: 구글 로그인 에러 해결, 인증 관련 패키지 다운그레이드

--- config/settings/default.py
+++ config/settings/default.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
 ]
 
 CORS_URLS_REGEX = r"^/api/.*$"
