18e9238ec88ede6604fd89bd155cdb67e671edbe
goddessana <twicegoddessana1229@gmail.com>
2023-10-10 13:44:17 +0900

    refactor: 권한 클래스 리팩토링 중

--- apps/studygroup/permissions.py
+++ apps/studygroup/permissions.py
@@ -5,7 +5,26 @@ from rest_framework.views import APIView
 from apps.studygroup.models import StudyGroup, StudyGroupMember
 
 
-class IsLeaderOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+class StudyGroupCreatePermission(permissions.IsAuthenticated):
+    """
+    스터디그룹을 생성할 수 있는 권한을 설정합니다.
+    """
+
+
+class StudyGroupDeleteOrUpdatePermission(permissions.BasePermission):
+    """
+    스터디그룹을 삭제하거나 수정할 수 있는 권한을 설정합니다.
+    - 스터디그룹의 리더만 가능
+    """
+
+    def has_object_permission(
+        self, request: Request, view: APIView, obj: StudyGroup
+    ) -> bool:
+        group_leaders = [leader.user for leader in obj.leaders]
+        return request.user in group_leaders
+
+
+class IsLeaderOrReadOnly(permissions.BasePermission):
     """
     스터디그룹에 대한 권한을 설정합니다.
     - 목록 보기, 상세 보기: 누구나 가능
@@ -22,6 +41,39 @@ class IsLeaderOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
         )
 
 
+class IsStudyGroupMember(permissions.BasePermission):
+    """
+    스터디그룹의 멤버인지 확인하는 권한을 설정합니다.
+    - 스터디그룹의 멤버만 가능
+    """
+
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        studygroup = StudyGroup.objects.get(uuid=view.kwargs["uuid"])
+        return request.user in [member.user for member in studygroup.members.all()]
+
+
+class IsStudyGroupLeader(permissions.BasePermission):
+    """
+    스터디그룹의 멤버 신청자를 조회할 수 있는 권한을 설정합니다.
+    - 스터디그룹의 리더만 가능
+    """
+
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        studygroup = StudyGroup.objects.get(uuid=view.kwargs["uuid"])
+        return request.user in [leader.user for leader in studygroup.leaders.all()]
+
+
+class StudyGroupMemberRead(permissions.BasePermission):
+    """
+    스터디그룹의 멤버를 조회할 수 있는 권한을 설정합니다.
+    - 스터디그룹에 참여하고 있는 멤버만 가능
+    """
+
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        studygroup = StudyGroup.objects.get(uuid=view.kwargs["uuid"])
+        return request.user in [member.user for member in studygroup.members.all()]
+
+
 class StudyGroupAddMember(permissions.BasePermission):
     """
     스터디그룹에 멤버를 추가할 수 있는 권한을 설정합니다.
@@ -30,8 +82,7 @@ class StudyGroupAddMember(permissions.BasePermission):
 
     def has_permission(self, request: Request, view: APIView) -> bool:
         """
-        - "현재 스터디그룹에 가입되어 있는 멤버를 조회" 하고자 하는 경우, 로그인한 유저는 스터디그룹의 멤버이거나 리더이어야 합니다.
-        - "현재 스더티그룹에 가입을 요청한 사람들을 조회" 하고자 하는 경우, 로그인한 유저는 리더이어야 합니다.
+        "현재 스터디그룹에 가입되어 있는 멤버를 조회" 하고자 하는 경우, 로그인한 유저는 스터디그룹의 멤버이거나 리더이어야 합니다.
         """
         studygroup = StudyGroup.objects.get(uuid=view.kwargs["uuid"])
         if request.query_params.get("is_approved") == "true":
