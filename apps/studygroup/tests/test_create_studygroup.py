6a0b34aecf90558cfb45b6baf53f99def790829d
goddessana <twicegoddessana1229@gmail.com>
2023-10-16 22:38:38 +0900

    test: 스터디그룹 카테고리를 무조건 하나 이상 강제하도록, 여러 개의 카테고리를 지정할 때의 유효성 검사, 생성 시 태그를 존재하지 않게 해도 가능하게 하는 검사, 태그 생성 or 가져오기를 수행하는 테스트 코드 추가, 스터디그룹 생성 시 개설자가 자동으로 스터디그룹장이 되는지 검증하는 테스트 코드 추가

--- apps/studygroup/tests/test_create_studygroup.py
+++ apps/studygroup/tests/test_create_studygroup.py
@@ -4,13 +4,14 @@ from django.utils.datetime_safe import date
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
-from apps.studygroup.models import StudyGroup
+from apps.studygroup.models import StudyGroup, Tag
 from apps.studygroup.tests.factories import CategoryFactory, UserFactory
 
 
 class CreateStudyGroupTestCase(APITestCase):
     def setUp(self) -> None:
         CategoryFactory(name="Django")
+        CategoryFactory(name="React")
         self.logged_in_user = UserFactory()
 
     def tearDown(self):
@@ -181,3 +182,159 @@ class CreateStudyGroupTestCase(APITestCase):
         response = self.client.post(url, data=data)
         self.assertEqual(response.status_code, 400, f"response: {response.data}")
         self.assertEqual(StudyGroup.objects.count(), 0, f"response: {response.data}")
+
+    def test_categories_is_required(self):
+        """
+        스터디그룹 카테고리는 필수입니다.
+        """
+        url = reverse("studygroup_list")
+        # 카테고리 없이 요청
+        data = {
+            "post_title": "스터디그룹 개설합니다.",
+            "post_content": "Django 스터디그룹입니다.",
+            "study_name": "Django 스터디",
+            "start_date": date.today() + timedelta(days=7),
+            "end_date": date.today() + timedelta(days=10),
+            "deadline": date.today() + timedelta(days=3),
+            "member_limit": 5,
+        }
+        self.client.force_authenticate(user=self.logged_in_user)
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, 400, f"response: {response.data}")
+        self.assertEqual(StudyGroup.objects.count(), 0, f"response: {response.data}")
+
+    def test_formdata_category_is_required(self):
+        """
+        formdata 로 여러 개의 카테고리를 생성하도록 요청할 때, 아래의 형식이어야 합니다.
+
+        categories = "Django"
+        categories = "React"
+        """
+
+        url = reverse("studygroup_list")
+        data = {
+            "post_title": "스터디그룹 개설합니다.",
+            "post_content": "Django 스터디그룹입니다.",
+            "study_name": "Django 스터디",
+            "start_date": date.today() + timedelta(days=7),
+            "end_date": date.today() + timedelta(days=10),
+            "deadline": date.today() + timedelta(days=3),
+            "member_limit": 5,
+            "categories": ["Django", "React"],
+        }
+        self.client.force_authenticate(user=self.logged_in_user)
+        response = self.client.post(url, data=data, format="multipart")
+        self.assertEqual(response.status_code, 201, f"response: {response.data}")
+        self.assertEqual(StudyGroup.objects.count(), 1, f"response: {response.data}")
+
+    def test_tag_can_be_empty(self):
+        """
+        스터디그룹 태그는 비워둘 수 있습니다.
+        """
+        url = reverse("studygroup_list")
+        data = {
+            "post_title": "스터디그룹 개설합니다.",
+            "post_content": "Django 스터디그룹입니다.",
+            "study_name": "Django 스터디",
+            "start_date": date.today() + timedelta(days=7),
+            "end_date": date.today() + timedelta(days=10),
+            "deadline": date.today() + timedelta(days=3),
+            "member_limit": 5,
+            "categories": "Django",
+        }
+        self.client.force_authenticate(user=self.logged_in_user)
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, 201, f"response: {response.data}")
+        self.assertEqual(StudyGroup.objects.count(), 1, f"response: {response.data}")
+
+    def test_tag_get(self):
+        """
+        이미 존재하는 태그를 요청하면, 새로운 태그를 생성하지 않습니다.
+        """
+        Tag.objects.create(name="some_tag")
+        url = reverse("studygroup_list")
+        data = {
+            "post_title": "스터디그룹 개설합니다.",
+            "post_content": "React 스터디그룹입니다.",
+            "study_name": "React 스터디",
+            "start_date": date.today() + timedelta(days=7),
+            "end_date": date.today() + timedelta(days=10),
+            "deadline": date.today() + timedelta(days=3),
+            "member_limit": 5,
+            "categories": "React",
+            "tags": "some_tag",
+        }
+        self.client.force_authenticate(user=self.logged_in_user)
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, 201, f"response: {response.data}")
+        self.assertEqual(StudyGroup.objects.count(), 1, f"response: {response.data}")
+        self.assertEqual(Tag.objects.count(), 1, f"response: {response.data}")
+
+    def test_tag_get_or_create(self):
+        """
+        스터디그룹 태그는 존재하지 않는 태그를 요청하면 생성됩니다.
+        """
+        url = reverse("studygroup_list")
+        data = {
+            "post_title": "스터디그룹 개설합니다.",
+            "post_content": "React 스터디그룹입니다.",
+            "study_name": "React 스터디",
+            "start_date": date.today() + timedelta(days=7),
+            "end_date": date.today() + timedelta(days=10),
+            "deadline": date.today() + timedelta(days=3),
+            "member_limit": 5,
+            "categories": "React",
+            "tags": "some_tag",
+        }
+        self.client.force_authenticate(user=self.logged_in_user)
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, 201, f"response: {response.data}")
+        self.assertEqual(StudyGroup.objects.count(), 1, f"response: {response.data}")
+        self.assertEqual(Tag.objects.count(), 1, f"response: {response.data}")
+
+    def test_create_with_tags(self):
+        """
+        스터디그룹 생성 시 태그를 여러 개 생성할 수 있습니다.
+        """
+        url = reverse("studygroup_list")
+        data = {
+            "post_title": "스터디그룹 개설합니다.",
+            "post_content": "React 스터디그룹입니다.",
+            "study_name": "React 스터디",
+            "start_date": date.today() + timedelta(days=7),
+            "end_date": date.today() + timedelta(days=10),
+            "deadline": date.today() + timedelta(days=3),
+            "member_limit": 5,
+            "categories": "React",
+            "tags": ["some_tag", "some_tag2"],
+        }
+        self.client.force_authenticate(user=self.logged_in_user)
+        response = self.client.post(url, data=data, format="multipart")
+        self.assertEqual(response.status_code, 201, f"response: {response.data}")
+        self.assertEqual(StudyGroup.objects.count(), 1, f"response: {response.data}")
+        self.assertEqual(Tag.objects.count(), 2, f"response: {response.data}")
+        self.assertEqual(response.data["tags"][0], data["tags"][0])
+        self.assertEqual(response.data["tags"][1], data["tags"][1])
+
+    def test_auto_registered_as_leader(self):
+        """
+        스터디그룹 개설자는 자동으로 스터디그룹장이 됩니다.
+        """
+        url = reverse("studygroup_list")
+        data = {
+            "post_title": "스터디그룹 개설합니다.",
+            "post_content": "Django 스터디그룹입니다.",
+            "study_name": "Django 스터디",
+            "start_date": date.today() + timedelta(days=7),
+            "end_date": date.today() + timedelta(days=10),
+            "deadline": date.today() + timedelta(days=3),
+            "member_limit": 5,
+            "categories": "Django",
+        }
+        self.client.force_authenticate(user=self.logged_in_user)
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, 201, f"response: {response.data}")
+        self.assertEqual(StudyGroup.objects.count(), 1, f"response: {response.data}")
+        self.assertEqual(
+            StudyGroup.objects.first().leaders.first().user, self.logged_in_user
+        )
