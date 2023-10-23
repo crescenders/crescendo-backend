e38dac649d300943908e06b6e92cd8b763defdf8
goddessana <twicegoddessana1229@gmail.com>
2023-10-23 23:35:21 +0900

    test: 태그 테스트 시, 태그 생성을 setUp 메서드에서 수행하는 것으로 변경. 간헐적으로 UNIQUE 제약 조건 에러 발생하는 것 방지

--- apps/studygroup/tests/test_read_random_tag_list.py
+++ apps/studygroup/tests/test_read_random_tag_list.py
@@ -7,11 +7,13 @@ from apps.studygroup.tests.factories import TagFactory
 
 
 class TagTestCase(APITestCase):
+    def setUp(self):
+        TagFactory.create_batch(10, name=factory.Faker("word"))
+
     def test_read_random_tags_default_is_3(self):
         """
         기본적으로 3개의 태그를 랜덤으로 반환합니다.
         """
-        TagFactory.create_batch(10, name=factory.Faker("word"))
         url = reverse("tag-list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -21,7 +23,6 @@ class TagTestCase(APITestCase):
         """
         querystring에 전달된 숫자만큼의 랜덤 태그를 반환합니다.
         """
-        TagFactory.create_batch(10, name=factory.Faker("word"))
         url = reverse("tag-list")
         response = self.client.get(url, {"random_count": 5})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -31,7 +32,6 @@ class TagTestCase(APITestCase):
         """
         querystring에 전달된 숫자가 범위를 벗어나면, 400 에러를 반환합니다.
         """
-        TagFactory.create_batch(10, name=factory.Faker("word"))
         url = reverse("tag-list")
         response = self.client.get(url, {"random_count": 11})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
