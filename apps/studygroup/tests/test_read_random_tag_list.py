cc99aad6b27c90664df3ed79b713cc072f182b1d
goddessana <twicegoddessana1229@gmail.com>
2023-10-23 19:07:44 +0900

    test: 랜덤 태그 API 테스트 코드 작성

---
+++ apps/studygroup/tests/test_read_random_tag_list.py
@@ -0,0 +1,37 @@
+import factory
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+from apps.studygroup.tests.factories import TagFactory
+
+
+class TagTestCase(APITestCase):
+    def test_read_random_tags_default_is_3(self):
+        """
+        기본적으로 3개의 태그를 랜덤으로 반환합니다.
+        """
+        TagFactory.create_batch(10, name=factory.Faker("word"))
+        url = reverse("tag-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
+    def test_read_random_tags_with_querystring(self):
+        """
+        querystring에 전달된 숫자만큼의 랜덤 태그를 반환합니다.
+        """
+        TagFactory.create_batch(10, name=factory.Faker("word"))
+        url = reverse("tag-list")
+        response = self.client.get(url, {"random_count": 5})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 5)
+
+    def test_read_random_tags_with_querystring_out_of_minimum_range(self):
+        """
+        querystring에 전달된 숫자가 범위를 벗어나면, 400 에러를 반환합니다.
+        """
+        TagFactory.create_batch(10, name=factory.Faker("word"))
+        url = reverse("tag-list")
+        response = self.client.get(url, {"random_count": 11})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
