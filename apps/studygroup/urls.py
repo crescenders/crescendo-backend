6a0828ace4fb62b5fd82ed99bf6658059a0b95f8
goddessana <twicegoddessana1229@gmail.com>
2023-10-25 23:24:48 +0900

    docs: studygroup 앱의 모든 url kwargs 의미있는 단어로 변경, SwaggerUI 개선

--- apps/studygroup/urls.py
+++ apps/studygroup/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from apps.studygroup.views.assignments import StudyGroupAssignmentRequestAPISet
+from apps.studygroup.views.assignments import (
+    StudyGroupAssignmentRequestAPISet,
+    StudyGroupAssignmentSubmissionAPISet,
+)
 from apps.studygroup.views.category import CategoryListAPI
 from apps.studygroup.views.members import (
     StudyGroupMemberDetailAPI,
@@ -19,29 +22,40 @@ assignment_router = SimpleRouter()
 assignment_router.register(
     r"assignments",
     StudyGroupAssignmentRequestAPISet,
-    basename="assignment",
+    basename="assignment-request",
+)
+
+assignment_submission_router = SimpleRouter()
+assignment_submission_router.register(
+    r"submissions",
+    StudyGroupAssignmentSubmissionAPISet,
+    basename="assignment-submission",
 )
 
 urlpatterns = [
     path("", include(studygroup_router.urls)),
-    path("studies/<uuid:uuid>/", include(assignment_router.urls)),
+    path("studies/<uuid:studygroup_uuid>/", include(assignment_router.urls)),
     path(
-        "studies/<uuid:uuid>/members/",
+        "studies/<uuid:studygroup_uuid>/<int:assignment_id>/",
+        include(assignment_submission_router.urls),
+    ),
+    path(
+        "studies/<uuid:studygroup_uuid>/members/",
         StudyGroupMemberListAPI.as_view(),
         name="studygroupmember-list",
     ),
     path(
-        "studies/<uuid:uuid>/members/<int:pk>/",
+        "studies/<uuid:studygroup_uuid>/members/<int:pk>/",
         StudyGroupMemberDetailAPI.as_view(),
         name="studygroupmember-detail",
     ),
     path(
-        "studies/<uuid:uuid>/requests/",
+        "studies/<uuid:studygroup_uuid>/requests/",
         StudyGroupMemberRequestListAPI.as_view(),
         name="studygroupmember-request-list",
     ),
     path(
-        "studies/<uuid:uuid>/requests/<int:pk>/",
+        "studies/<uuid:studygroup_uuid>/requests/<int:pk>/",
         StudyGroupMemberRequestDetailAPI.as_view(),
         name="studygroupmember-request-detail",
     ),
