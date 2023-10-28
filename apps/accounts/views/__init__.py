e50e7d6631c8598b07fdf97e949bdc6afdf8a37b
goddessana <twicegoddessana1229@gmail.com>
2023-10-29 03:14:11 +0900

    refactor: view Import 방식에 MyStudyAPI 추가

--- apps/accounts/views/__init__.py
+++ apps/accounts/views/__init__.py
@@ -1,12 +1,13 @@
 from apps.accounts.views.login import GoogleLoginAPI
 from apps.accounts.views.logout import LogoutAPI
-from apps.accounts.views.profile import MyProfileAPI, UUIDProfileAPI
+from apps.accounts.views.profile import MyProfileAPI, MyStudyAPI, UUIDProfileAPI
 from apps.accounts.views.refresh import TokenRefreshAPI
 
 __all__ = [
     "GoogleLoginAPI",
     "LogoutAPI",
     "MyProfileAPI",
+    "MyStudyAPI",
     "UUIDProfileAPI",
     "TokenRefreshAPI",
 ]
