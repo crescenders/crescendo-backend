d3c978b04292dc823452fd7114626b7731dfdd0d
goddessana <twicegoddessana1229@gmail.com>
2023-10-29 03:13:42 +0900

    refactor: urls.py 구조 개선, URL 들을 dataclass 로 분리하여 관리

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,22 +1,80 @@
+from dataclasses import dataclass
+
 from django.urls import include, path
 
-from apps.accounts import views
+from apps.accounts.views import (
+    GoogleLoginAPI,
+    LogoutAPI,
+    MyProfileAPI,
+    MyStudyAPI,
+    TokenRefreshAPI,
+    UUIDProfileAPI,
+)
+
+
+@dataclass(frozen=True)
+class AccountsURLs:
+    SIGNUP: str = "signup"
+    REFRESH_LOGIN: str = "refresh-login"
+    GOOGLE_LOGIN: str = "google-login"
+    LOGOUT: str = "token-blacklist"
+    MY_PROFILE: str = "profile-login-user"
+    MY_PROFILE_STUDIES: str = "profile-login-studies"
+    UUID_PROFILE: str = "profile-uuid"
+
 
 urlpatterns = [
     # 회원가입
-    path("signup/", include("allauth.urls"), name="socialaccount_signup"),
-    # 로그인
-    path("login/refresh/", views.TokenRefreshAPI.as_view(), name="token_refresh"),
-    path("login/google/", views.GoogleLoginAPI.as_view(), name="google_login"),
-    # 로그아웃
-    path("logout/", views.LogoutAPI.as_view(), name="token_blacklist"),
-    # 내 정보 조회/수정/탈퇴
-    path("profiles/me/", views.MyProfileAPI.as_view(), name="user_profile_me"),
-    path("profiles/me/studies/", views.MyStudyAPI.as_view(), name="user_study_me"),
-    # UUID 기반 유저 정보 조회
     path(
-        "profiles/<uuid:user_uuid>/",
-        views.UUIDProfileAPI.as_view(),
-        name="user_profile_uuid",
+        "signup/",
+        include("allauth.urls"),
+        name="socialaccount-signup",
+    ),
+    # 로그인
+    path(
+        "login/",
+        include(
+            [
+                path(
+                    "refresh/",
+                    TokenRefreshAPI.as_view(),
+                    name="refresh-login",
+                ),
+                path(
+                    "google/",
+                    GoogleLoginAPI.as_view(),
+                    name="google-login",
+                ),
+            ]
+        ),
+    ),
+    # 로그아웃
+    path(
+        "logout/",
+        LogoutAPI.as_view(),
+        name="token-blacklist",
+    ),
+    # 프로필
+    path(
+        "profiles/",
+        include(
+            [
+                path(
+                    "me/",
+                    MyProfileAPI.as_view(),
+                    name="profile-login-user",
+                ),
+                path(
+                    "me/studies/",
+                    MyStudyAPI.as_view(),
+                    name="profile-login-studies",
+                ),
+                path(
+                    "<uuid:user_uuid>/",
+                    UUIDProfileAPI.as_view(),
+                    name="profile-uuid",
+                ),
+            ]
+        ),
     ),
 ]
