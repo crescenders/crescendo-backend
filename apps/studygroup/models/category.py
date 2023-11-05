454f99e93a302873183b99e20e9dc94a58cd5d28
goddessana <twicegoddessana1229@gmail.com>
2023-11-05 11:39:12 +0900

    feat: 카테고리 모델 이름 번역할 수 있도록 gettext 사용

--- apps/studygroup/models/category.py
+++ apps/studygroup/models/category.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
 
 
 class Category(models.Model):
     class Meta:
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
 
     name = models.CharField(max_length=20, unique=True)
 
