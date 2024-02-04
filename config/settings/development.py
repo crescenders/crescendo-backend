9957f69d6fefb41bd9c57b348b0cc6ba1fe193fb
goddessana <twicegoddessana1229@gmail.com>
2024-02-04 15:48:56 +0900

    feat: 개발 환경에서 SQL 출력 설정 추가

--- config/settings/development.py
+++ config/settings/development.py
@@ -28,3 +28,26 @@ REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
     "rest_framework.renderers.JSONRenderer",
     "rest_framework.renderers.BrowsableAPIRenderer",
 ]
+
+if os.environ.get("ECHO_SQL", False):
+    LOGGING = {
+        "version": 1,
+        "filters": {
+            "require_debug_true": {
+                "()": "django.utils.log.RequireDebugTrue",
+            }
+        },
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "filters": ["require_debug_true"],
+                "class": "logging.StreamHandler",
+            }
+        },
+        "loggers": {
+            "django.db.backends": {
+                "level": "DEBUG",
+                "handlers": ["console"],
+            }
+        },
+    }
